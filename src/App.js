4262525f8146402b34dbe5252404aba1ba105d82
JimLee12 <qkq010177@gmail.com>
2022-10-25 19:00:19 +0900

    saaaq

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import WeatherMain from './com/WeatherMain';
 import WeatherMain2 from './com/WeatherMain2';
 import {BrowserRouter, Route, Router, Routes} from "react-router-dom";
-import Nav from './com/Nav';
 import Home from './com/Home';
 
 // import Home from './pages/Home'
